99f245a77b7a03730ecadf010c58df8ea26d79f1
DaVinciKollektiv <118380549+DaVinciKollektiv@users.noreply.github.com>
2024-12-14 00:16:18 +0100

    Redis Kennwort Anpassung

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = os.getenv('REDIS_PORT', '6379')
 REDIS_DB = os.getenv('REDIS_DB', '0')
 
-REDIS_URL = f"redis://default:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+REDIS_URL = f"redis://{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
 # Configure Celery
 celery = Celery(
